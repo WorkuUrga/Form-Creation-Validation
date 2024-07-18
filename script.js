36549d30a08be18133ef48a98ac903207b49eb10
WorkuUrga <urgaworku@gmail.com>
2024-07-18 11:01:59 +0300

    corrections on script

--- script.js
+++ script.js
@@ -6,13 +6,13 @@ feedbackDiv = document.getElementById("form-feedback");
 form.addEventListener('submit', function (event){
     event.preventDefault()
 
-    const userName = document.getElementById('username').value.trim();
+    const username = document.getElementById('username').value.trim();
     const email = document.getElementById('email').value.trim();
     const password = document.getElementById('password').value.trim();
 
     let isValid = true;
     const messages = [];
-    if (userName.length < 3) {
+    if (username.length < 3) {
         isValid = false;
         messages.push("Username must be at least 3 characters long.");
     }
