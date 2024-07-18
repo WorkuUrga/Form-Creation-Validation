283cf8b099b001a79ab310ed3238f0e4cec0d805
WorkuUrga <urgaworku@gmail.com>
2024-07-18 12:02:03 +0300

    Fetching Data from an API and Displaying It

---
+++ fetch-data.js
@@ -0,0 +1,25 @@
+
+
+const fetchUserData = async ()=> {
+    const apiUrl = 'https://jsonplaceholder.typicode.com/users';
+    const dataContainer = document.getElementById("api-data");
+    
+    try {
+        const response = await fetch(apiUrl);
+        const users = await response.json();
+        dataContainer.innerHTML = ''
+        const userList = document.createElement("ul")
+
+        users.forEach((user) => {
+            const userName = document.createElement('li')
+            userName.textContent = user.name;
+            userList.appendChild(userName);
+        });
+        dataContainer.appendChild(userList);
+    }
+    catch(error) {
+        dataContainer = '';
+        dataContainer.textContent = 'Failed to load user data.';
+    }
+}
+document.addEventListener('DOMContentLoaded', fetchUserData);
